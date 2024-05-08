cc11d5797a3522f93d791f97a1010e22cb5a61b4
imre-berente <imre.berente@gmail.com>
2024-05-08 13:27:55 +0200

    more tests, reorganizing, finding need for more electric power

---
+++ servo_test/servo_test.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+
+Servo servo0;
+int dir = 1;
+int target = 0;
+
+constexpr unsigned int SERVO_PIN = 9;
+
+void setup() {
+  servo0.attach(SERVO_PIN);
+//  Serial.begin(9600);
+}
+
+void loop() {
+//  Serial.println(target);
+  servo0.write(target);
+  target+=dir;
+  if ((target == 0) || (target == 180)) dir=-dir;
+
+  delay(50);
+
+}
